8d51625bd5f3dcea21ae47ed4ef5ec96ece5d3a8
jmlouro <joao.m.louro@parceiros.nos.pt>
2024-04-17 14:31:15 +0100

    adds certs

---
+++ Certifications/C# Masterclass - Udemy/HelloWorld/HelloWorld/Box.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    public  class Box
+    {
+        public int Width { get; set; }
+
+        public Box() 
+        { 
+            
+        }
+
+        public static void SayMyName()
+        {
+            Console.WriteLine("Barrriga");
+        }
+    }
+}
